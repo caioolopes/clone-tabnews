2acf7815f4916834b9c407d2e56b0782c40798fc
caioolopes <122275651+caioolopes@users.noreply.github.com>
2025-05-31 22:49:32 +0000

    adds `/api/v1/migrations` wndpoint

---
+++ teste/integration/api/v1/migrations/pust.test.js
@@ -0,0 +1,26 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+test("PUST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const response1Bory = await response1.json();
+  expect(Array.isArray(response1Bory)).toBe(true);
+  expect(response1Bory.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Bory = await response2.json();
+  expect(Array.isArray(response2Bory)).toBe(true);
+  expect(response2Bory.length).toBe(0);
+});
